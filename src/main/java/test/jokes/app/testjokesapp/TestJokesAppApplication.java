86637986ff628dd0d6a6a164960da6645f47b84a
gitkeshav89 <gitkeshava@gmail.com>
2021-12-22 16:54:56 +0530

    NEW

---
+++ src/main/java/test/jokes/app/testjokesapp/TestJokesAppApplication.java
@@ -0,0 +1,13 @@
+package test.jokes.app.testjokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJokesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestJokesAppApplication.class, args);
+	}
+
+}
